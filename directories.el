85bc309a374aa77f48144e2428be7af8a19a3fad
Joseph LaFreniere <joseph@lafreniere.xyz>
2020-04-11 12:57:55 -0500

    Relicense as GPL3+

--- directories.el
+++ directories.el
@@ -1,25 +1,39 @@
 ;;; directories.el --- Provide platform-specific paths for configuration, cache, and other data
-;;
+
 ;; Copyright (C) 2018 Joseph M LaFreniere
-;;
+
 ;; Author: Joseph M LaFreniere <joseph@lafreniere.xyz>
 ;; Maintainer: Joseph M LaFreniere <joseph@lafreniere.xyz>
-;; License: ISC
+;; License: GPL3+
 ;; URL: https://github.com/lafrenierejm/directories-elisp
 ;; Version: 0.0.1
 ;; Keywords: files
-;;
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;; The purpose of this package is to provide platform-specfic, paths for reading
 ;; and writing configuration, cache, and other data.
-;;
+
 ;; On Linux (`gnu/linux'), the directory paths conform to the XDG base directory
 ;; and XDG user directory specifications as published by the freedesktop.org
 ;; project.
-;;
+
 ;; On macOS (`darwin'), the directory paths conform to Apple's documentation at
 ;; https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPFileSystem/Articles/WhereToPutFiles.html.
-;;
+
 ;;; Code:
 (eval-when-compile
   (require 'env)
